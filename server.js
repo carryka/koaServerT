00c8b1f25d286ffa4bfdf451391646fa70575034
carryka <65294219+carryka@users.noreply.github.com>
2021-03-07 12:45:01 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const static = require("koa-static");
+
+let app = new Koa();
+
+app.listen(8080);
+console.log("The server has ran in 8080 port");
+
+let router = new Router();
+
+router.get("/",async ctx=>{
+    //ctx.body = "111";
+    ctx.throw(400,"your error");
+})
+
+app.use(router.routes());
+
+app.use(static("./static",{
+    maxAge:86400*1000,
+
+}));
